76c3bb55e9bea32beae691102d835ecc6c3c2000
Ziya Feyyaz <ziya.feyyaz@yandex.ru>
2016-05-12 07:38:50 +0300

    Modify cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # private
+  private
 
-  #   def current_cart 
-  #     Cart.find(session[:cart_id])
-  #   rescue ActiveRecord::RecordNotFound
-  #     cart = Cart.create
-  #     session[:cart_id] = cart.id
-  #     cart
-  #   end
+    def current_cart 
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,4 @@
 class StoreController < ApplicationController
-  # skip_before_action :authorize
-  # include CurrentCart
-  # before_action :set_cart
-  
   def index
     @products = Product.order(:title)
   end
